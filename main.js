7a17a187e2153a68893f3cd11d08d5acb9d0189d
Isabella Correa <isabelitacorreacarvajal@gmail.com>
2019-09-20 08:50:52 -0500

    los mensajes se cargany se limpia el input

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 
-
-document.getElementById('btnAdd').addEventListener('click', function (){
+document.getElementById('btnAdd').addEventListener('click', function(){
     var div = document.createElement('div');
     var str = document.createTextNode(document.getElementById('textChat').value);
+    div.className = 'messages';
     div.appendChild(str);
-    var chat = document.getElementById('textChat');
-    //document.body.insertBefore(div,chat);
-
-
+    var chat = document.getElementById('message');
+    document.getElementById('chat').insertBefore(div,chat);
+    document.getElementById('textChat').value = "";
 });
